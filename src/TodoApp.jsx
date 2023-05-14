97d47fbde47234766eff03ab937f1f263836d874
mvrk <oscarmenacho23@gmail.com>
2023-05-13 23:50:29 -0500

    consumir el api mediante el customHook

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,11 +1,23 @@
+import { useGetTodosQuery } from './store/apis/todosApi'
+
 export const TodoApp = () => {
+  const { data: todos = [], isLoading } = useGetTodosQuery()
+
   return (
     <>
       <h1>Todos - RTK Query</h1>
       <hr />
-      <h4>isLoading...</h4>
+      <h4>isLoading: {isLoading ? 'true' : 'false'}</h4>
 
       <pre>...</pre>
+      <ul>
+        {todos.map(({ title, id, completed }) => (
+          <li key={id}>
+            <strong>{completed ? 'DONE' : 'Pending'}</strong>
+            {title}
+          </li>
+        ))}
+      </ul>
 
       <button>Next Todo</button>
     </>
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { counterSlice } from './slices/counter'
 import { pokemonSlice } from './slices/pokemon/'
+import { todosApi } from './apis/todosApi'
 
 export const store = configureStore({
   reducer: {
     counter: counterSlice.reducer,
     pokemons: pokemonSlice.reducer,
+
+    [todosApi.reducerPath]: todosApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(todosApi.middleware),
 })
