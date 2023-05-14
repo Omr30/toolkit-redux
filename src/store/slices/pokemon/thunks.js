247e2a937cd15e6cdd51ab57461ae07bc0b76a72
mvrk <oscarmenacho23@gmail.com>
2023-05-13 22:18:45 -0500

    thunks

---
+++ src/store/slices/pokemon/thunks.js
@@ -0,0 +1,9 @@
+import { startLoadingPokemons } from './pokemonSlice'
+
+export const getPokemons = (page = 0) => {
+  return async (dispatch, getState) => {
+    dispatch(startLoadingPokemons())
+
+    // dispatch(setPokemons())
+  }
+}
